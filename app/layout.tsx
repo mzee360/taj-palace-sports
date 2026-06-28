288683f0c71eb6064852212fe7c02d6c37c790b1
mzee360 <mzee360@gmail.com>
2026-06-28 17:41:59 +0600

    Delete app directory

--- app/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function RootLayout({ children }) {
-  return (
-    <html>
-      <body style={{ fontFamily: "Arial", margin: 0 }}>
-        {children}
-      </body>
-    </html>
-  );
-}
